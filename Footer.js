0a798a0a0e67896e4208a06bcd80cd8b0a8ef386
Rishabhmathur2311 <107494585+Rishabhmathur2311@users.noreply.github.com>
2023-02-09 20:13:31 +0530

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+var date = new Date();
+var year = date.getFullYear();
+
+function Footer() {
+  return (
+    <footer>
+      <p>copyright {year}</p>
+    </footer>
+  );
+}
+
+export default Footer;
